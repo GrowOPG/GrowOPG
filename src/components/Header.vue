f4a51b77fa109fc062868ae71dff514eb8155412
David <60017346+DavidMaglica@users.noreply.github.com>
2020-12-29 15:52:00 +0100

    Added Header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <span class="header col-12">
+        <strong>GrowOPG</strong>
+        <div class="dropdown-divider"></div>
+    </span>
+</template>
+
+<style lang='scss'>
+.header{
+    text-align: center;    
+    font-size: 50px;
+    color:black;
+}
+.dropdown-divider {
+    height: 0;
+    margin: .5rem 0;
+    overflow: hidden;
+    border-top: 2px solid #2D2D2D;
+}
+</style>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
