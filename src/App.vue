018c714cf348bfd00c6cb66441386af79c023d43
David <60017346+DavidMaglica@users.noreply.github.com>
2020-12-29 20:31:48 +0100

    bemu miša krivo sam komentar stavio

--- src/App.vue
+++ src/App.vue
@@ -42,8 +42,8 @@ export default {
             .signOut()
             .then(() => {
                 this.$router.push({name: 'login'})
-            }); */
-        }
+            }); 
+        }*/
     }
 }
 </script>
