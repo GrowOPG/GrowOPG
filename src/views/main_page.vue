5fe8a533680929e799bdf6ce1718d2a9cd447ca7
David <60017346+DavidMaglica@users.noreply.github.com>
2020-12-29 20:32:17 +0100

    Update main_page.vue

--- src/views/main_page.vue
+++ src/views/main_page.vue
@@ -1,16 +1,41 @@
 <template>
-  <div class="main_page">
-    <h1>This is our main page</h1>
-  </div>
-</template>
+    <div class="bg">
+        
 
+
+        <Footer />
+    </div>
+</template>
 
 <style scoped>
 
 </style>
 
 <script>
+import app from '@/App';
+import store from '@/store';
+import Footer from '../components/Footer.vue';
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in.
+    console.log(user.email);
+    store.currentUser = user.email;
+  }
+  else {
+    // User is not signed in
+    store.currentUser = null;
+    // if the user is not signed in as is not on our 'Home' he will be redirected there
+    if (router.name !== "home"){
+        router.push({name:'home'})
+    }
+  }
+});
+
 export default {
-    
-}
+   name: 'main_page',
+   components: {
+      Footer
+   }
+};
 </script>
