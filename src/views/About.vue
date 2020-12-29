968efca21941d60b5104c1d52cbfa945f1dbd4c9
David <60017346+DavidMaglica@users.noreply.github.com>
2020-12-29 15:59:41 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,7 +16,7 @@ import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
 export default {
-   name: 'login',
+   name: 'about',
    components: {
        Footer,
        Header
