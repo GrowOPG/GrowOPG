eef1a8e8a7330a600713bc95af17bec27b67268c
David <60017346+DavidMaglica@users.noreply.github.com>
2020-12-29 15:58:13 +0100

    Inserted Header + Footer

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="bg">
+    <Header />
+
+
+    <Footer />
+    </div>
 </template>
+
+<style scoped>
+
+</style>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+   name: 'login',
+   components: {
+       Footer,
+       Header
+   }
+};
+</script>
