0ce3b50037022b70ea0b4a7b0a167425916ac00c
Rrrovik <46111371+Rrrovik@users.noreply.github.com>
2020-12-30 13:40:21 +0100

    homepage fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,120 +1,57 @@
 <template>
-    <div class="container no-gutters no-padding">
+    <div class="container-fluid no-gutters no-padding">
         <div class="row no-gutters justify-content-around">
-            <div class="col-3 no-padding">
+            
+            
+            <div class="col-lg-4 choice">
                 <router-link to="/login">
                     <img src="@/assets/black-bg.jpg" style="width: 100%; height: 100vh;">
-                </router-link>
-                    
-                    <div class="buyer-text"><strong>I am a</strong></div>
-                    <div class="buyer-text2"><strong>BUYER</strong></div>
                 
-            </div>
+                    <div class="side-text">
+                        <strong class="smaller-title">I am a</strong>
+                        <h2 class="small-title">BUYER</h2>
+                    </div>
 
-            <div class="col-6">
+                </router-link>
+            </div>
+            
+
+            <div class="col-lg-4">
 
                 <img src="@/assets/wood-bg.jpg" style="width: 100%; height: 100vh;" >
 
                 <div class="logo"> 
-                    <img class="resize" src="@/assets/grow-opg-logo.png">
+                    <img class="resize" src="@/assets/logo.png">
                 </div>
 
-                <div class="desc"> 
-                   <strong>Welcome to GrowOPG</strong>
+                <div class="text-box-centered"> 
+                   <h1 class="main-title">Welcome to GrowOPG</h1>
+                   <p class="excerpt">Who are we? Who is a buyer? itd itd itd itd itd itd tiriirt titiit tititi</p>
                 </div>
 
-                <div class="desc2">
-                    Who are we? Who is a buyer? itd itd itd itd itd itd tiriirt titiit tititi
-                </div>
+                    
+
             </div>
 
-            <div class="col-3">
+            
+            <div class="col-lg-4 choice">
                 <router-link to="/login">
                     <img src="@/assets/grey-bg.jpg" style="width: 100%; height: 100vh;">
+
+                <div class="side-text">
+                        <strong class="smaller-title black">I am a</strong>
+                        <h2 class="small-title black">SELLER</h2>
+                    </div>
+
                 </router-link>
-                                   
-                <div class="seller-text"><strong>I am a</strong></div>
-                <div class="seller-text2"><strong>SELLER</strong></div>
-
-
             </div>
+            
         </div>       
     </div>
 </template>
 
 <style scoped>
-.container { /*so that theres no margin on the right*/
-    margin-right: 0;
-}
-.dropdown-divider {
-    height: 0;
-    margin: .5rem 0;
-    overflow: hidden;
-    border-top: 2px solid white;
-}
-.buyer-text { /* */
-    position: absolute;
-    top: 45%;
-    left: 32%;
-    transform: translate(-50%, -50%);
-    font-size: 40px;
-    color: white;
-}
-.buyer-text2 { /* */
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 70px;
-    color: white;
-}
-.logo { /* */
-    position: absolute;
-    top: 20%;
-    left: 41%;
-    /* transform: translate(-50%, -50%); */
-}
-img.resize { /*needed to resize logo*/
-    width: 30%;
-    height: 30%;
-}
-.desc { /* */
-    position: absolute;
-    top: 35%;
-    left: 15%;
-    right: 15%;
-    /* transform: translate(-50%, -50%); */
-    text-align: center;    
-    font-size: 35px;
-    color: white;
-}
-.desc2 { /* */
-    position: absolute;
-    top: 45%;
-    left: 10%;
-    right: 10%;
-    /* transform: translate(-50%, -50%); */
-    text-align: justify;
-    text-align: center;
-    font-size: 25px;
-    color: white;
-}
-.seller-text { /* */
-    position: absolute;
-    top: 45%;
-    left: 32%;
-    transform: translate(-50%, -50%);
-    font-size: 40px;
-    color: #2D2D2D;
-}
-.seller-text2 { /* */
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 70px;
-    color: #2D2D2D;
-}
+
 .no-padding { /*no padding on the column/row -- found on stack-overflow*/
     padding-left: 0;
     padding-right: 0;
@@ -123,6 +60,65 @@ img.resize { /*needed to resize logo*/
     margin-left: 0;
     margin-right: 0;
 }
+
+.text-box-centered { /* */
+    position: absolute;
+    top: 35%;
+    left: 15%;
+    right: 15%;
+    text-align: center;    
+}
+
+.side-text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    transition: 0.3s ease-out;
+}
+
+.side-text:hover {
+    filter: drop-shadow(16px 16px 10px black);
+}
+
+.small-title {
+    color: white;
+    font-size: 4rem;
+    font-weight: bold;
+}
+
+.smaller-title {
+    color: white;
+    font-size: 3rem;
+    font-weight: bold;
+}
+
+.logo {
+    position: absolute;
+    top: 10%;
+    left: 34%;
+}
+img.resize { /*needed to resize logo*/
+    width: 50%;
+    height: 50%;
+}
+
+.main-title {
+    color: white;
+    font-size: 3rem;
+    font-weight: bolder;
+}
+
+.excerpt {
+    color: white;
+    font-size: 1.2rem;
+}
+
+.black {
+    color: black;
+}
+
 </style>
 
 <script>
