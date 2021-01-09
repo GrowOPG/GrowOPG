159591a2d19b111083ea890e0126f3040d83a15f
David <60017346+DavidMaglica@users.noreply.github.com>
2021-01-09 12:58:22 +0100

    main page with main header

--- src/views/main-page.vue
+++ src/views/main-page.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg">
-        
+        <MainHeader />
 
 
         <Footer />
@@ -14,28 +14,29 @@
 <script>
 import app from '@/App';
 import store from '@/store';
-import Footer from '../components/Footer.vue';
-/*
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in.
-    console.log(user.email);
-    store.currentUser = user.email;
-  }
-  else {
-    // User is not signed in
-    store.currentUser = null;
-    // if the user is not signed in and is not on our 'Home' he will be redirected there
-    if (router.name !== "home"){
-        router.push({name:'home'})
-    }
-  }
-});
-*/
+import MainHeader from '../components/Main-Header';
+import Footer from '../components/Footer';
+
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//     // User is signed in.
+//     store.currentUser = user.email;
+//   }
+//   else {
+//     // User is not signed in
+//     store.currentUser = null;
+//     // if the user is not signed in and is not on our 'Home' he will be redirected there
+//     if (router.name !== "home"){
+//         router.push({name:'home'})
+//     }
+//   }
+// });
+
 export default {
    name: 'main-page',
    components: {
-      Footer
+        MainHeader,
+        Footer
    }
 };
 </script>
