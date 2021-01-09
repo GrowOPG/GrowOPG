6f496b8d90309b1d8e37ff74910ea9fff8b294f6
Rrrovik <maurovicmarin@gmail.com>
2021-01-09 15:25:27 +0100

    main page first section

--- src/views/main-page.vue
+++ src/views/main-page.vue
@@ -1,14 +1,75 @@
 <template>
     <div class="bg">
+
         <MainHeader />
 
+        <div class="row-fluid">
+            <div class="flex-container">
+
+                <div class="centered">
+                    <div class="category-filter">
+                        <img class="resize" src="@/assets/cheese.png">
+                        <span class="caption">Cheese</span>
+                    </div>
+
+                    <div class="category-filter">
+                        <img class="resize" src="@/assets/egg.png">
+                        <span class="caption">Egg</span>
+                    </div>
+
+                    <div class="category-filter">
+                        <img class="resize" src="@/assets/fruits.png">
+                        <span class="caption">Fruits</span>
+                    </div>
+
+                    <div class="category-filter">
+                        <img class="resize" src="@/assets/honey.png">
+                        <span class="caption">Honey</span>
+                    </div>
+
+                    <div class="category-filter">
+                        <img class="resize" src="@/assets/olive-oil.png">
+                        <span class="caption">Olive Oil</span>
+                    </div>
+
+                    <div class="category-filter">
+                        <img class="resize" src="@/assets/vegetables.png">
+                        <span class="caption">Vegetables</span>
+                    </div>
+
+                    <div class="category-filter">
+                        <img class="resize" src="@/assets/wine.png">
+                        <span class="caption">Wine</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
         <Footer />
+        
     </div>
 </template>
 
 <style scoped>
 
+.category-filter {
+    vertical-align: top;
+    display: inline-block;
+    text-align: center;
+    margin-right: 3%;
+}
+
+.caption {
+    display: block;
+    margin-top: 2px;
+    font-weight: bold;
+}
+
+.centered {
+    text-align: center;
+    margin-top: 5%;
+}
 </style>
 
 <script>
