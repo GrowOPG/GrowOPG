709acd9a18b88629dea450f763c34a5f740fc8bd
Gabriel <gmikulaco@unipu.hr>
2021-04-05 19:43:11 +0200

    storage options added

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 var firebaseConfig = {
     apiKey: "AIzaSyBiDzOP1SCfSaBh7BPQnu5o83azZ_FFDpk",
